90fedbe7d24e186b5016008ad1e7e6e98fc740e8
Nyito <49852550+Nyito@users.noreply.github.com>
2024-08-23 12:02:00 -0300

    Revert "Adjust Upload"
    This reverts commit d8f49e6848a0961fc1ed4f0332aeb256968ae672.

--- app/controllers/uploadController.js
+++ app/controllers/uploadController.js
@@ -11,7 +11,7 @@ exports.uploadFile = async (req, res) => {
             originalName: req.file.originalname, 
             type: req.file.type,
             size: req.file.size,
-            author: req.user.name,
+            author: req.user._id,
             tags: req.body.tags ? req.body.tags.split(',').map(tag => tag.trim()) : [],
             accessLevel: req.body.accessLevel || 'public'
         });
@@ -27,7 +27,7 @@ exports.uploadFile = async (req, res) => {
 
 exports.listFiles = async (req, res) => {
     try {
-        const files = await File.find({}).populate('author', 'name email');
+        const files = await File.find({});
 
         res.status(200).json({
             files: files.map(file => ({
